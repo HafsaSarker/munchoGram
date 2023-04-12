3187d3b45f6182f4b9ebb2ad52e9b0f1d614bd0a
Hafsa Sarker <73364680+HafsaSarker@users.noreply.github.com>
2023-04-11 23:21:39 -0400

    create nav bar

--- munchoGram/src/components/Navbar/Navbar.jsx
+++ munchoGram/src/components/Navbar/Navbar.jsx
@@ -3,6 +3,8 @@ import './Navbar.css'
 export default function Navbar(){
     return (
         <nav>
+            <h1>munchoGram</h1>
+            <input type='text' placeholder='search' />
             <ul>
                 <li>Home</li>
                 <li>Create New Post</li>
