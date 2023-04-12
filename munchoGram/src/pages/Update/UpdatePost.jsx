035aedd4c446cde53e392ea7a4fe3b2e367b7b09
Hafsa Sarker <73364680+HafsaSarker@users.noreply.github.com>
2023-04-12 01:36:22 -0400

    crate editPost page

--- munchoGram/src/pages/Update/UpdatePost.jsx
+++ munchoGram/src/pages/Update/UpdatePost.jsx
@@ -3,7 +3,13 @@ import './UpdatePost.css'
 export default function UpdatePost(){
     return (
         <div className="update-post">
-            UpdatePost
+            <h3>Edit Post</h3>
+            <form className='update-post-form'>
+                <input type="text" placeholder='Title' required/>
+                <textarea placeholder='Content (Optional)' />
+                <input type='text' placeholder='Image URL (Optional)' />
+                <button>Edit</button>
+            </form>
         </div>
     )
 }
