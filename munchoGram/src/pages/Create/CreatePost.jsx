4832f69d83807a0cd5cd1ecd368c96ffebc9bea7
Hafsa Sarker <73364680+HafsaSarker@users.noreply.github.com>
2023-04-11 23:55:48 -0400

    create post page

--- munchoGram/src/pages/Create/CreatePost.jsx
+++ munchoGram/src/pages/Create/CreatePost.jsx
@@ -3,7 +3,13 @@ import './CreatePost.css'
 export default function CreatePost(){
     return (
         <div className="create-post">
-            CreatePost
+            <h3>Create Post</h3>
+            <form className='create-post-form'>
+                <input type="text" placeholder='Title' required/>
+                <textarea placeholder='Content (Optional)' />
+                <input type='text' placeholder='Image URL (Optional)' />
+                <button>Create</button>
+            </form>
         </div>
     )
 }
