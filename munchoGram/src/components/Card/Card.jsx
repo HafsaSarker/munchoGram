c9fa82b16c472f1ea33612f8ed2bc964c92a5b0b
Hafsa Sarker <73364680+HafsaSarker@users.noreply.github.com>
2023-04-12 02:07:43 -0400

    home page created

---
+++ munchoGram/src/components/Card/Card.jsx
@@ -0,0 +1,13 @@
+import './Card.css'
+
+export default function Card() {
+    return (
+        <div className="card">
+            <p>Posted 20 hours ago</p>
+
+            <h4>What is your favorite Korean food? </h4>
+
+            <p>3 upvotes</p>
+        </div>
+    )
+}
--- munchoGram/src/pages/Home/HomeFeed.jsx
+++ munchoGram/src/pages/Home/HomeFeed.jsx
@@ -1,9 +1,21 @@
 import './HomeFeed.css'
+import Card from '../../components/Card/Card'
 
 export default function HomeFeed(){
     return (
         <div className="home-feed">
-            HomeFeed
+           <div className="sort">
+            <p>Order by: </p>
+            <p className='filter-btn'>Newest</p>
+            <p className='filter-btn'>Most Popular</p>
+           </div>
+
+           <div className="card-container">
+                <Card />
+
+                <Card />
+            </div>
+
         </div>
     )
 }
