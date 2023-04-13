a04c3073ab8e4427e34a35f087f838c085d3df79
Hafsa Sarker <73364680+HafsaSarker@users.noreply.github.com>
2023-04-13 00:28:16 -0400

    add link to edit btn

--- munchoGram/src/pages/Read/ReadPost.jsx
+++ munchoGram/src/pages/Read/ReadPost.jsx
@@ -2,6 +2,7 @@ import './ReadPost.css'
 import { BsArrowReturnRight } from 'react-icons/bs'
 import { AiOutlineHeart, AiFillHeart } from 'react-icons/ai'
 import { MdOutlineEdit, MdDeleteOutline } from 'react-icons/md'
+import { Link } from 'react-router-dom'
 
 export default function ReadPost(){
     return (
@@ -14,13 +15,28 @@ export default function ReadPost(){
                 <div className="post-stats">
                     <p className='upvotes'><span className='icon'><AiOutlineHeart/></span>3</p>
                     <div className="update-icons">
-                        <span className="icon"><MdOutlineEdit/></span>
+                        <Link to='/editPost'>
+                            <span className="icon">
+                                <MdOutlineEdit/>
+                            </span>
+                        </Link>
+
                         <span className="icon"><MdDeleteOutline/></span>
                     </div>
                 </div>
                 <div className="comments-container">
-                    <p><span className='icon'><BsArrowReturnRight/></span>omg! How did you make that?</p>
-                    <p><span className='icon'><BsArrowReturnRight/></span>watching this at 3am is not a good feeling</p>
+                    <p>
+                        <span className='icon'>
+                            <BsArrowReturnRight/>
+                        </span>
+                        omg! How did you make that?
+                    </p>
+                    <p>
+                        <span className='icon'>
+                            <BsArrowReturnRight/>
+                        </span>
+                        watching this at 3am is not a good feeling
+                    </p>
                     <input type="text" placeholder='Leave a comment'/>
                 </div>
             </div>
