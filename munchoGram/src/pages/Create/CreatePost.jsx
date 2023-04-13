d720bdefdf844b929da9812506e5111586546bd9
Hafsa Sarker <73364680+HafsaSarker@users.noreply.github.com>
2023-04-13 01:14:09 -0400

    add form handling to createPost

--- munchoGram/src/pages/Create/CreatePost.jsx
+++ munchoGram/src/pages/Create/CreatePost.jsx
@@ -1,13 +1,46 @@
+import { useState } from 'react'
 import './CreatePost.css'
 
 export default function CreatePost(){
+    const [newPost, setNewPost] = useState({
+        title: "",
+        content: null,
+        url: null
+    });
+    const handleChange = (e) => {
+        setNewPost((prev) => ({
+            ...prev,
+            [e.target.name]: e.target.value
+        }));
+    }
+
+    const createPost = (e) => {
+        e.preventDefault();
+
+        console.log(newPost);
+    }
     return (
         <div className="create-post">
             <h3>Create Post</h3>
-            <form className='create-post-form'>
-                <input type="text" placeholder='Title' required/>
-                <textarea placeholder='Content (Optional)' />
-                <input type='text' placeholder='Image URL (Optional)' />
+            <form className='create-post-form' onSubmit={createPost}>
+                <input 
+                    type="text" 
+                    name='title'
+                    placeholder='Title'
+                    onChange={handleChange} 
+                    required
+                />
+                <textarea 
+                    placeholder='Content (Optional)'
+                    name='content'
+                    onChange={handleChange}
+                />
+                <input 
+                    type='text' 
+                    placeholder='Image URL (Optional)' 
+                    name='url'
+                    onChange={handleChange}
+                    />
                 <button>Create</button>
             </form>
         </div>
